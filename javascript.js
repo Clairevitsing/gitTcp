b84d04549c6ba05244cb1e9d765657da4219d417
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-04-25 11:31:10 +0200

    deuxième version ajouter une bouton

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var favoriteButton = document.querySelector('.favorite');
+
+    favoriteButton.addEventListener('click', function() {
+        // Simuler l'ajout aux favoris (ceci est juste un exemple)
+        console.log("Ajouté aux favoris!");
+        alert("Ajouté aux favoris!");
+    });
+});
