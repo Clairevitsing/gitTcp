5ab2d3a709d5af6819941d6bad340f74245dbe38
claire <115070408+Clairevitsing@users.noreply.github.com>
2024-04-25 11:41:44 +0200

    creer branch1

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var favoriteButton = document.querySelector(".favorite");
 
-document.addEventListener("DOMContentLoaded", function() {
-    var favoriteButton = document.querySelector('.favorite');
-
-    favoriteButton.addEventListener('click', function() {
-        // Simuler l'ajout aux favoris (ceci est juste un exemple)
-        console.log("Ajouté aux favoris!");
-        alert("Ajouté aux favoris!");
-    });
+  favoriteButton.addEventListener("click", function () {
+    // Simuler l'ajout aux favoris (ceci est juste un exemple)
+    console.log("Ajouté aux favoris!");
+    alert("Vous n'avez pas de droit de le mofitifier!");
+  });
 });
+
+function registerUser() {
+  alert("Formulaire d'inscription ouvert!");
+  console.log("Processus d'inscription initié.");
+}
